56de3f722873ffb481a4139b86e526a647d2c6ce
Manuel Rubio <manuel@altenwald.com>
2025-06-26 13:39:01 +0200

    publish 0.1.0

--- mix.exs
+++ mix.exs
@@ -1,22 +1,23 @@
 defmodule Paypal.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :paypal,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: docs()
-    ]
-  end
-
-  defp docs do
-    [
-      # before_closing_head_tag: &before_closing_head_tag/1,
-      before_closing_body_tag: &mermaid/1
+      name: "Paypal",
+      description: "Paypal API v2",
+      docs: docs(),
+      package: package(),
+      preferred_cli_env: [
+        check: :test
+      ]
     ]
   end
 
@@ -76,6 +77,69 @@ defmodule Paypal.MixProject do
       {:ex_check, "~> 0.14", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Paypal",
+      source_ref: "v#{@version}",
+      canonical: "https://hexdocs.pm/paypal",
+      # logo: "guides/images/paypal.png",
+      # extra_section: "GUIDES",
+      source_url: "https://github.com/altenwald/paypal",
+      # extras: extras(),
+      # groups_for_extras: groups_for_extras(),
+      # before_closing_head_tag: &before_closing_head_tag/1,
+      before_closing_body_tag: &mermaid/1,
+      groups_for_modules: [
+        Auth: [
+          Paypal.Auth,
+          Paypal.Auth.Access,
+          Paypal.Auth.Request,
+          Paypal.Auth.Worker
+        ],
+        Order: [
+          Paypal.Order,
+          Paypal.Order.Authorization,
+          Paypal.Order.Authorized,
+          Paypal.Order.Create,
+          Paypal.Order.ExperienceContext,
+          Paypal.Order.Info,
+          Paypal.Order.Payer,
+          Paypal.Order.PurchaseUnit,
+          Paypal.Order.PurchaseUnit.Capture,
+          Paypal.Order.PurchaseUnit.Item,
+          Paypal.Order.PurchaseUnit.PaymentCollection,
+          Paypal.Order.UpcCode
+        ],
+        Payment: [
+          Paypal.Payment,
+          Paypal.Payment.Captured,
+          Paypal.Payment.Info,
+          Paypal.Payment.Refund,
+          Paypal.Payment.RefundRequest
+        ],
+        "Common and Helpers": [
+          Paypal.Common.CurrencyValue,
+          Paypal.Common.Error,
+          Paypal.Common.Link,
+          Paypal.Common.Operation,
+          Paypal.EctoHelpers
+        ]
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w[ lib mix.* *.md COPYING ],
+      maintainers: ["Manuel Rubio"],
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/altenwald/paypal",
+        "Docs" => "https://hexdocs.pm/paypal"
+      }
     ]
   end
 end
